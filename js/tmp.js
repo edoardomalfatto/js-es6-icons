58d08c52a09957efb7e14956edb685424285db2a
Edoardo <edoardo.malfatto@hotmail.it>
2021-03-18 17:47:04 +0100

    milestone1 terminata

---
+++ js/tmp.js
@@ -0,0 +1,122 @@
+const icons = [{
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+];
+
+/* definire un array di oggetti; ogni oggetto
+rappresenta un'icona, che è caratterizzata da:
+nome, prefisso, tipo e famiglia.
+Utilizzando la funzione forEach e il template
+literal, visualizzare in pagina tutte le icone con il
+proprio nome. */
+
+
+const container = $(".icons");
+
+container.html("");
+//1) Inserisco le icone nel container
+
+icons.forEach((item) => {
+    const { name, prefix, family } = item;
+    const elementHTML = `
+	<div>
+	<i class="${family} ${prefix}${name}"></i>
+	<div class="titolo">${name.toUpperCase()}</div>
+	</div>`;
+
+    container.append(elementHTML);
+
+});
